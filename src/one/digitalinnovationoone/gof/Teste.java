cc061c23a7ff8a08ceb6e505dd93583b566318c8
chritianegozza <christiane_natacha@hotmail.com>
2022-06-01 00:52:24 -0300

    Subindo projeto

---
+++ src/one/digitalinnovationoone/gof/Teste.java
@@ -0,0 +1,29 @@
+package one.digitalinnovationoone.gof;
+
+public class Teste {
+
+    public static void main(String[] args) {
+        SingletonLazy lazy = SingletonLazy.getInstance();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager  =  SingletonEager.getInstance();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstance();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+
+
+    }
+}
+
+
+
+
+
+
